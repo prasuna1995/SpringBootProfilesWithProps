bf2449a0cf566ad2a99fc741c5e54e23a2967f91
user <user@DESKTOP-DFDQVV4>
2019-06-09 20:38:58 +0530

    another pgm

---
+++ src/main/java/com/app/SpringBootProfilesWithPropsApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesWithPropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesWithPropsApplication.class, args);
+	}
+
+}
